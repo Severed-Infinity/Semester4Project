cd05f3494e82763e5b45871f54b3a80d6033b3cd
Severed-Infinity <masterdavidswift@hotmail.com>
2014-02-28 17:22:30 +0000

    Added the Update Views
    added the updateCourse, updateModule, and updateUser to the main view.
    Formatted the code.

--- Semester4Project/src/com/project/gui/TimetableView.java
+++ Semester4Project/src/com/project/gui/TimetableView.java
@@ -52,6 +52,23 @@ import javax.swing.border.TitledBorder;
     private JTextField textField_14;
     private JTextField textField_15;
     private JTextField textField_16;
+    private JTextField textField_17;
+    private JTextField textField_18;
+    private JTextField textField_19;
+    private JTextField textField_20;
+    private JTextField textField_21;
+    private JTextField textField_22;
+    private JTextField textField_23;
+    private JTextField textField_24;
+    private JTextField textField_25;
+    private JTextField textField_26;
+    private JTextField textField_27;
+    private JTextField textField_28;
+    private JTextField textField_29;
+    private JTextField textField_30;
+    private JTextField textField_31;
+    private JTextField textField_32;
+    private JTextField textField_33;
 
     public TimetableView () throws HeadlessException {
 
@@ -148,6 +165,30 @@ import javax.swing.border.TitledBorder;
         JPanel mainView = new JPanel();
         getContentPane().add(mainView, BorderLayout.CENTER);
         mainView.setLayout(new CardLayout(0, 0));
+
+        JPanel timetableView = new JPanel();
+        mainView.add(timetableView, "name_109576600023605");
+        timetableView.setLayout(new GridLayout(5, 1, 5, 0));
+
+        JPanel monday = new JPanel();
+        timetableView.add(monday);
+        monday.setLayout(new GridLayout(0, 8, 5, 0));
+
+        JPanel tuseday = new JPanel();
+        timetableView.add(tuseday);
+        tuseday.setLayout(new GridLayout(0, 8, 5, 0));
+
+        JPanel wednesday = new JPanel();
+        timetableView.add(wednesday);
+        wednesday.setLayout(new GridLayout(0, 8, 5, 0));
+
+        JPanel thursday = new JPanel();
+        timetableView.add(thursday);
+        thursday.setLayout(new GridLayout(0, 8, 0, 0));
+
+        JPanel friday = new JPanel();
+        timetableView.add(friday);
+        friday.setLayout(new GridLayout(0, 8, 5, 0));
 
         JTabbedPane adminView = new JTabbedPane(JTabbedPane.TOP);
         mainView.add(adminView, "name_173771604594465");
@@ -246,30 +287,6 @@ import javax.swing.border.TitledBorder;
         gbc_panel_5.gridx = 1;
         gbc_panel_5.gridy = 2;
         User.add(panel_5, gbc_panel_5);
-
-        JPanel timetableView = new JPanel();
-        mainView.add(timetableView, "name_109576600023605");
-        timetableView.setLayout(new GridLayout(5, 1, 5, 0));
-
-        JPanel monday = new JPanel();
-        timetableView.add(monday);
-        monday.setLayout(new GridLayout(0, 8, 5, 0));
-
-        JPanel tuseday = new JPanel();
-        timetableView.add(tuseday);
-        tuseday.setLayout(new GridLayout(0, 8, 5, 0));
-
-        JPanel wednesday = new JPanel();
-        timetableView.add(wednesday);
-        wednesday.setLayout(new GridLayout(0, 8, 5, 0));
-
-        JPanel thursday = new JPanel();
-        timetableView.add(thursday);
-        thursday.setLayout(new GridLayout(0, 8, 0, 0));
-
-        JPanel friday = new JPanel();
-        timetableView.add(friday);
-        friday.setLayout(new GridLayout(0, 8, 5, 0));
 
         JPanel addNewCourse = new JPanel();
         addNewCourse.setLayout(null);
@@ -637,6 +654,360 @@ import javax.swing.border.TitledBorder;
         textField_4.setBounds(467, 88, 120, 20);
         addUserView.add(textField_4);
         textField_4.setColumns(10);
+
+        JPanel updateCourse = new JPanel();
+        updateCourse.setLayout(null);
+        updateCourse.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"),
+
+        "Add New Course", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+        mainView.add(updateCourse, "name_183053656053264");
+
+        JLabel label_8 = new JLabel("Type of Course");
+        label_8.setBounds(35, 33, 95, 14);
+        updateCourse.add(label_8);
+
+        JComboBox<String> comboBox_10 = new JComboBox<String>();
+        comboBox_10.setBounds(140, 30, 120, 20);
+        updateCourse.add(comboBox_10);
+
+        JLabel label_10 = new JLabel("Name");
+        label_10.setBounds(35, 66, 95, 14);
+        updateCourse.add(label_10);
+
+        textField_17 = new JTextField();
+        textField_17.setColumns(10);
+        textField_17.setBounds(140, 63, 120, 20);
+        updateCourse.add(textField_17);
+
+        JLabel label_12 = new JLabel("Course Code");
+        label_12.setBounds(350, 66, 95, 14);
+        updateCourse.add(label_12);
+
+        textField_18 = new JTextField();
+        textField_18.setColumns(10);
+        textField_18.setBounds(467, 63, 120, 20);
+        updateCourse.add(textField_18);
+
+        JLabel label_16 = new JLabel("ID");
+        label_16.setBounds(35, 91, 95, 14);
+        updateCourse.add(label_16);
+
+        textField_19 = new JTextField();
+        textField_19.setColumns(10);
+        textField_19.setBounds(140, 88, 120, 20);
+        updateCourse.add(textField_19);
+
+        JLabel label_17 = new JLabel("Department");
+        label_17.setBounds(35, 116, 95, 14);
+        updateCourse.add(label_17);
+
+        JLabel label_18 = new JLabel("Course Length");
+        label_18.setBounds(35, 144, 95, 14);
+        updateCourse.add(label_18);
+
+        textField_20 = new JTextField();
+        textField_20.setColumns(10);
+        textField_20.setBounds(140, 141, 120, 20);
+        updateCourse.add(textField_20);
+
+        JLabel label_19 = new JLabel("Select Module");
+        label_19.setBounds(35, 191, 95, 14);
+        updateCourse.add(label_19);
+
+        JComboBox<String> comboBox_11 = new JComboBox<String>();
+        comboBox_11.setBounds(140, 188, 192, 20);
+        updateCourse.add(comboBox_11);
+
+        JButton button_4 = new JButton("Add Module");
+        button_4.setBounds(350, 188, 95, 23);
+        updateCourse.add(button_4);
+
+        JButton button_8 = new JButton("Confirm");
+        button_8.setBounds(467, 360, 89, 23);
+        updateCourse.add(button_8);
+
+        JButton button_9 = new JButton("Cancel");
+        button_9.setBounds(665, 360, 89, 23);
+        updateCourse.add(button_9);
+
+        JButton button_10 = new JButton("Clear");
+        button_10.setBounds(566, 360, 89, 23);
+        updateCourse.add(button_10);
+
+        JLabel label_20 = new JLabel("Modules: ");
+        label_20.setBounds(35, 216, 95, 14);
+        updateCourse.add(label_20);
+
+        JList<String> list_3 = new JList<String>();
+        list_3.setBounds(140, 214, 192, 125);
+        updateCourse.add(list_3);
+
+        JLabel label_21 = new JLabel("Number of Semesters");
+        label_21.setBounds(350, 144, 110, 14);
+        updateCourse.add(label_21);
+
+        JLabel label_22 = new JLabel("Password");
+        label_22.setBounds(350, 91, 95, 14);
+        updateCourse.add(label_22);
+
+        textField_21 = new JTextField();
+        textField_21.setColumns(10);
+        textField_21.setBounds(467, 88, 120, 20);
+        updateCourse.add(textField_21);
+
+        textField_22 = new JTextField();
+        textField_22.setColumns(10);
+        textField_22.setBounds(467, 141, 120, 20);
+        updateCourse.add(textField_22);
+
+        JComboBox<String> comboBox_12 = new JComboBox<String>();
+        comboBox_12.setBounds(140, 113, 192, 20);
+        updateCourse.add(comboBox_12);
+
+        JButton button_11 = new JButton("Create New Module");
+        button_11.setBounds(467, 188, 127, 23);
+        updateCourse.add(button_11);
+
+        JPanel updateModule = new JPanel();
+        updateModule.setLayout(null);
+        updateModule.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"),
+
+        "Add New Module", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+        mainView.add(updateModule, "name_183096271792642");
+
+        JLabel label_23 = new JLabel("Type of Course");
+        label_23.setBounds(35, 33, 95, 14);
+        updateModule.add(label_23);
+
+        JComboBox<String> comboBox_13 = new JComboBox<String>();
+        comboBox_13.setBounds(140, 30, 120, 20);
+        updateModule.add(comboBox_13);
+
+        JLabel label_24 = new JLabel("Name");
+        label_24.setBounds(35, 66, 95, 14);
+        updateModule.add(label_24);
+
+        textField_23 = new JTextField();
+        textField_23.setColumns(10);
+        textField_23.setBounds(140, 63, 120, 20);
+        updateModule.add(textField_23);
+
+        JLabel label_25 = new JLabel("Module Code");
+        label_25.setBounds(350, 66, 95, 14);
+        updateModule.add(label_25);
+
+        textField_24 = new JTextField();
+        textField_24.setColumns(10);
+        textField_24.setBounds(467, 63, 120, 20);
+        updateModule.add(textField_24);
+
+        JLabel label_26 = new JLabel("ID");
+        label_26.setBounds(35, 91, 95, 14);
+        updateModule.add(label_26);
+
+        textField_25 = new JTextField();
+        textField_25.setColumns(10);
+        textField_25.setBounds(140, 88, 120, 20);
+        updateModule.add(textField_25);
+
+        JLabel label_27 = new JLabel("Department");
+        label_27.setBounds(35, 116, 95, 14);
+        updateModule.add(label_27);
+
+        JLabel label_28 = new JLabel("Hours a Week");
+        label_28.setBounds(350, 165, 95, 14);
+        updateModule.add(label_28);
+
+        textField_26 = new JTextField();
+        textField_26.setColumns(10);
+        textField_26.setBounds(467, 162, 120, 20);
+        updateModule.add(textField_26);
+
+        JLabel label_29 = new JLabel("Select Lecturer");
+        label_29.setBounds(35, 191, 95, 14);
+        updateModule.add(label_29);
+
+        JComboBox<String> comboBox_14 = new JComboBox<String>();
+        comboBox_14.setBounds(140, 188, 192, 20);
+        updateModule.add(comboBox_14);
+
+        JButton button_12 = new JButton("Add Lecturer");
+        button_12.setBounds(350, 187, 95, 23);
+        updateModule.add(button_12);
+
+        JButton button_13 = new JButton("Confirm");
+        button_13.setBounds(467, 360, 89, 23);
+        updateModule.add(button_13);
+
+        JButton button_14 = new JButton("Cancel");
+        button_14.setBounds(665, 360, 89, 23);
+        updateModule.add(button_14);
+
+        JButton button_15 = new JButton("Clear");
+        button_15.setBounds(566, 360, 89, 23);
+        updateModule.add(button_15);
+
+        JLabel label_30 = new JLabel("Lecturers: ");
+        label_30.setBounds(35, 216, 95, 14);
+        updateModule.add(label_30);
+
+        JList<String> list_4 = new JList<String>();
+        list_4.setBounds(140, 214, 192, 125);
+        updateModule.add(list_4);
+
+        JLabel label_31 = new JLabel("Number of Semesters");
+        label_31.setBounds(350, 138, 110, 14);
+        updateModule.add(label_31);
+
+        JLabel label_32 = new JLabel("Password");
+        label_32.setBounds(350, 91, 95, 14);
+        updateModule.add(label_32);
+
+        textField_27 = new JTextField();
+        textField_27.setColumns(10);
+        textField_27.setBounds(467, 88, 120, 20);
+        updateModule.add(textField_27);
+
+        textField_28 = new JTextField();
+        textField_28.setColumns(10);
+        textField_28.setBounds(467, 138, 120, 20);
+        updateModule.add(textField_28);
+
+        JComboBox<String> comboBox_15 = new JComboBox<String>();
+        comboBox_15.setBounds(140, 113, 192, 20);
+        updateModule.add(comboBox_15);
+
+        JButton button_16 = new JButton("Create New Course");
+        button_16.setBounds(467, 187, 127, 23);
+        updateModule.add(button_16);
+
+        JLabel label_33 = new JLabel("Course");
+        label_33.setBounds(35, 141, 95, 14);
+        updateModule.add(label_33);
+
+        JComboBox<String> comboBox_16 = new JComboBox<String>();
+        comboBox_16.setBounds(140, 138, 120, 20);
+        updateModule.add(comboBox_16);
+
+        JLabel label_34 = new JLabel("Course Code: ");
+        label_34.setBounds(35, 165, 225, 14);
+        updateModule.add(label_34);
+
+        JPanel updateUser = new JPanel();
+        updateUser.setLayout(null);
+        updateUser.setBorder(new TitledBorder(UIManager.getBorder("TitledBorder.border"),
+
+        "Add New User", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+        mainView.add(updateUser, "name_183142460631028");
+
+        JLabel label_35 = new JLabel("Type of User");
+        label_35.setBounds(35, 33, 95, 14);
+        updateUser.add(label_35);
+
+        JComboBox<String> comboBox_17 = new JComboBox<String>();
+        comboBox_17.setBounds(140, 30, 120, 20);
+        updateUser.add(comboBox_17);
+
+        JLabel label_36 = new JLabel("Name");
+        label_36.setBounds(35, 66, 95, 14);
+        updateUser.add(label_36);
+
+        textField_29 = new JTextField();
+        textField_29.setColumns(10);
+        textField_29.setBounds(140, 63, 120, 20);
+        updateUser.add(textField_29);
+
+        JLabel label_37 = new JLabel("Title");
+        label_37.setBounds(350, 66, 95, 14);
+        updateUser.add(label_37);
+
+        textField_30 = new JTextField();
+        textField_30.setColumns(10);
+        textField_30.setBounds(467, 63, 120, 20);
+        updateUser.add(textField_30);
+
+        JLabel label_38 = new JLabel("ID");
+        label_38.setBounds(35, 91, 95, 14);
+        updateUser.add(label_38);
+
+        textField_31 = new JTextField();
+        textField_31.setColumns(10);
+        textField_31.setBounds(140, 88, 120, 20);
+        updateUser.add(textField_31);
+
+        JLabel label_39 = new JLabel("Department");
+        label_39.setBounds(35, 116, 95, 14);
+        updateUser.add(label_39);
+
+        JComboBox<String> comboBox_18 = new JComboBox<String>();
+        comboBox_18.setBounds(140, 113, 192, 20);
+        updateUser.add(comboBox_18);
+
+        JLabel label_40 = new JLabel("Course");
+        label_40.setBounds(35, 141, 95, 14);
+        updateUser.add(label_40);
+
+        JComboBox<String> comboBox_19 = new JComboBox<String>();
+        comboBox_19.setBounds(140, 138, 120, 20);
+        updateUser.add(comboBox_19);
+
+        JLabel label_41 = new JLabel("Course Code: ");
+        label_41.setBounds(35, 166, 225, 14);
+        updateUser.add(label_41);
+
+        JLabel label_42 = new JLabel("Years");
+        label_42.setBounds(350, 141, 62, 14);
+        updateUser.add(label_42);
+
+        textField_32 = new JTextField();
+        textField_32.setColumns(10);
+        textField_32.setBounds(467, 138, 120, 20);
+        updateUser.add(textField_32);
+
+        JLabel label_43 = new JLabel("Teaches");
+        label_43.setBounds(35, 191, 95, 14);
+        updateUser.add(label_43);
+
+        JComboBox<String> comboBox_20 = new JComboBox<String>();
+        comboBox_20.setBounds(140, 188, 192, 20);
+        updateUser.add(comboBox_20);
+
+        JButton button_17 = new JButton("Add Module");
+        button_17.setBounds(350, 187, 95, 23);
+        updateUser.add(button_17);
+
+        JButton button_18 = new JButton("Confirm");
+        button_18.setBounds(467, 360, 89, 23);
+        updateUser.add(button_18);
+
+        JButton button_19 = new JButton("Cancel");
+        button_19.setBounds(665, 360, 89, 23);
+        updateUser.add(button_19);
+
+        JButton button_20 = new JButton("Clear");
+        button_20.setBounds(566, 360, 89, 23);
+        updateUser.add(button_20);
+
+        JLabel label_44 = new JLabel("Modules: ");
+        label_44.setBounds(35, 216, 95, 14);
+        updateUser.add(label_44);
+
+        JList<String> list_5 = new JList<String>();
+        list_5.setBounds(140, 214, 192, 125);
+        updateUser.add(list_5);
+
+        JLabel label_45 = new JLabel("Number of Semesters: ");
+        label_45.setBounds(350, 166, 237, 14);
+        updateUser.add(label_45);
+
+        JLabel label_46 = new JLabel("Password");
+        label_46.setBounds(350, 91, 95, 14);
+        updateUser.add(label_46);
+
+        textField_33 = new JTextField();
+        textField_33.setColumns(10);
+        textField_33.setBounds(467, 88, 120, 20);
+        updateUser.add(textField_33);
 
         // TODO Auto-generated constructor stub
     }
