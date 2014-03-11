6e6b00929074044575903758f695850489f8f93f
Severed-Infinity <masterdavidswift@hotmail.com>
2014-03-11 16:49:56 +0000

    Added new package and Intellij support
    added a package for database classes and source.
    added an abstracted class called Views for all the views contained in
    the main view.
    created the main class from which the application will be launched.
    added support for Intellij.

---
+++ Semester4Project/src/TimetableMain.java
@@ -0,0 +1,16 @@
+
+
+public final class TimetableMain {
+
+    public TimetableMain () {
+
+        // TODO Auto-generated constructor stub
+    }
+
+    public static void main (String[] args) {
+
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ Semester4Project/src/com/project/gui/View.java
@@ -0,0 +1,22 @@
+package com.project.gui;
+
+import javax.swing.*;
+
+@SuppressWarnings ("serial") public abstract class View extends JPanel {
+
+    protected JTextField tf;
+    protected JButton btn;
+    protected JLabel lbl;
+    protected JPanel pnl;
+    protected JComboBox<Object> cbx;
+    protected JList<Object> lst;
+//    protected JPasswordField passwordField;
+
+    public View () {
+
+        super();
+
+        // TODO Auto-generated constructor stub
+    }
+
+}
