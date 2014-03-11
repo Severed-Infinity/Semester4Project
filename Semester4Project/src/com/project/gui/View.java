60819418b8acd0287187d15e3a921d776a2a710d
Severed-Infinity <masterdavidswift@hotmail.com>
2014-03-11 19:52:55 +0000

    Minor code formatting and imports
    some minor code organising was done and improved imports.
    added some dynamic content to the View class.

--- Semester4Project/src/com/project/gui/View.java
+++ Semester4Project/src/com/project/gui/View.java
@@ -1,8 +1,10 @@
 package com.project.gui;
 
 import javax.swing.*;
+import java.awt.*;
 
-@SuppressWarnings ("serial") public abstract class View extends JPanel {
+@SuppressWarnings ("serial")
+public abstract class View extends JPanel {
 
     protected JTextField tf;
     protected JButton btn;
@@ -10,11 +12,15 @@ import javax.swing.*;
     protected JPanel pnl;
     protected JComboBox<Object> cbx;
     protected JList<Object> lst;
-//    protected JPasswordField passwordField;
+    protected JFrame parent;
 
-    public View () {
+    // protected JPasswordField passwordField;
 
-        super();
+    public View ( JPanel panelName, Component position, String title ) {
+        
+        parent = (JFrame) SwingUtilities.getWindowAncestor( this );
+        parent.setTitle( title );
+        parent.getContentPane().add( panelName, position );
 
         // TODO Auto-generated constructor stub
     }
--- Semester4Project/src/com/project/gui/WarningPanes.java
+++ Semester4Project/src/com/project/gui/WarningPanes.java
@@ -1,6 +1,6 @@
 package com.project.gui;
 
-import javax.swing.JOptionPane;
+import javax.swing.*;
 
 public class WarningPanes {
 
@@ -9,22 +9,15 @@ public class WarningPanes {
         // TODO Auto-generated constructor stub
     }
 
-    public static void main (String[] args) {
+    public static void main ( String[] args ) {
 
-        JOptionPane.showMessageDialog(null, "User ID or Password is incorrect", null,
-                JOptionPane.WARNING_MESSAGE, null);
-        JOptionPane.showMessageDialog(null, "Search returned no results", null,
-                JOptionPane.WARNING_MESSAGE, null);
-        JOptionPane.showMessageDialog(null, "Please select a Course", null,
-                JOptionPane.WARNING_MESSAGE, null);
-        JOptionPane.showMessageDialog(null, "Please select a Module", null,
-                JOptionPane.WARNING_MESSAGE, null);
-        JOptionPane.showMessageDialog(null, "Please select a User", null,
-                JOptionPane.WARNING_MESSAGE, null);
-        JOptionPane.showConfirmDialog(null, "Are you sure you want to clear all fields?", null,
-                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
-        JOptionPane.showConfirmDialog(null, "Are you sure you want to add to the database", null,
-                JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE);
+        JOptionPane.showMessageDialog( null, "User ID or Password is incorrect", null, JOptionPane.WARNING_MESSAGE, null );
+        JOptionPane.showMessageDialog( null, "Search returned no results", null, JOptionPane.WARNING_MESSAGE, null );
+        JOptionPane.showMessageDialog( null, "Please select a Course", null, JOptionPane.WARNING_MESSAGE, null );
+        JOptionPane.showMessageDialog( null, "Please select a Module", null, JOptionPane.WARNING_MESSAGE, null );
+        JOptionPane.showMessageDialog( null, "Please select a User", null, JOptionPane.WARNING_MESSAGE, null );
+        JOptionPane.showConfirmDialog( null, "Are you sure you want to clear all fields?", null, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE );
+        JOptionPane.showConfirmDialog( null, "Are you sure you want to add to the database", null, JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE );
     }
 
 }
